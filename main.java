d2f786d281e96f570ce6745b58a35c4915f3d64c
jahirubc <jahirubc@gmail.com>
2022-10-25 16:15:40 +0600

    first commit

---
+++ main.java
@@ -0,0 +1,5 @@
+public class Main{
+    public static vaid mian(string args[]){
+        System.out.println("Hello World");
+    }
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ git init
 git status
 git add .\filename
 git rm --cached "filename"
+git commit --m "done"
